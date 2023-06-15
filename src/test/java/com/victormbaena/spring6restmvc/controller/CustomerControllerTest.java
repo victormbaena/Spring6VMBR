547c7bf9d963f20311b57ef6e048756e15df98b6
Victor M. Baena <portalegre66@gmail.com>
2023-06-15 14:47:49 +0100

    [Victor M. Baena] - Section 7 about the Spring MockMVC add Delete and Patch tests method.

--- src/test/java/com/victormbaena/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/com/victormbaena/spring6restmvc/controller/CustomerControllerTest.java
@@ -6,17 +6,24 @@ import com.victormbaena.spring6restmvc.services.CustomerService;
 import com.victormbaena.spring6restmvc.services.CustomerServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -33,9 +40,63 @@ public class CustomerControllerTest {
 
     CustomerServiceImpl customerServiceImpl;
 
+    @Captor
+    ArgumentCaptor<UUID> uuidArgumentCaptor;
+
+    @Captor
+    ArgumentCaptor<Customer> customerArgumentCaptor;
+
     @BeforeEach
     void setUp() {
         customerServiceImpl = new CustomerServiceImpl();
+    }
+
+    @Test
+    void patchCustomerTest() throws Exception {
+        Customer customer = customerServiceImpl.listCustomer().get(0);
+        Map<String, Object> customerMap = new HashMap<>();
+        customerMap.put("customerName", "New name");
+
+        mockMvc.perform(patch("/api/v1/customer/" + customer.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customerMap)))
+                .andExpect(status().isNoContent());
+
+        verify(customerService)
+                .patchCustomerById(uuidArgumentCaptor.capture(), customerArgumentCaptor.capture());
+
+        assertThat(customer.getId())
+                .isEqualTo(uuidArgumentCaptor.getValue());
+        assertThat(customerMap.get("customerName"))
+                .isEqualTo(customerArgumentCaptor.getValue().getCustomerName());
+    }
+
+    @Test
+    void deleteCustomerTest() throws Exception {
+        Customer customer = customerServiceImpl.listCustomer().get(0);
+
+        mockMvc.perform(delete("/api/v1/customer/" + customer.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        verify(customerService).deleteCustomerById(uuidArgumentCaptor.capture());
+
+        assertThat(customer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+
+    }
+
+    @Test
+    void updateCustomerTest() throws Exception {
+        Customer customer = customerServiceImpl.listCustomer().get(0);
+
+        mockMvc.perform(put("/api/v1/customer/" + customer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer)))
+                .andExpect(status().isNoContent());
+
+        verify(customerService).updateCustomerById(any(UUID.class), any(Customer.class));
     }
 
     @Test
@@ -48,9 +109,9 @@ public class CustomerControllerTest {
                 .willReturn(customerServiceImpl.listCustomer().get(1));
 
         mockMvc.perform(post("/api/v1/customer")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(customer)))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"));
     }
@@ -66,6 +127,7 @@ public class CustomerControllerTest {
                 .andExpect(jsonPath("$.length()", is(3)));
 
     }
+
     @Test
     void getCustomerByIdTest() throws Exception {
         Customer testCustomer = customerServiceImpl.listCustomer().get(0);
